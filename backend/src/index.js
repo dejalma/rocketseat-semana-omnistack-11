dfbcbfbc373b389069358516d947bc48af4682e7
dejalma <dejalma.arantes@gmail.com>
2020-03-25 13:32:18 -0300

    app backend

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+// import express from 'express'
+// import routes from './routes.js'
+const express = require('express')
+const cors = require('cors')
+
+const { routes } = require('./routes.js')
+
+var app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333)
