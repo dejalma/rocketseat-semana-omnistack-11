fb7b91ed69f4b0a7f4ccd943d346a2cf51b1ce0e
dejalma <dejalma.arantes@gmail.com>
2020-03-28 13:55:24 -0300

    fronfend react done

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import './styles.css'
 
 import LogoImg from '../../assets/logo.svg'
 
 export default function NewIncident() {
+
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [value, setValue] = useState('')
+
+  const history = useHistory()
+
+  const ongId = localStorage.getItem('ongId')
+
+  async function handleNewIncident(e) {
+    e.preventDefault()
+
+    const data = {
+      title,
+      description,
+      value
+    }
+
+    try {
+      await api.post('/incidents', data, {
+        headers: {
+          Authorization: ongId
+        }
+      })
+
+      history.push('/profile')
+    } catch(error) {
+      alert(`Erro ao cadastrar caso. ${error}`)
+    }
+  }
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -23,14 +56,27 @@ export default function NewIncident() {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="Título do caso" />
-          <textarea placeholder="Descriçao" />
-          <input placeholder="Valor (R$)" />
+        <form onSubmit={handleNewIncident}>
+          <input
+            placeholder="Título do caso"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Descriçao"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
+          <input
+            placeholder="Valor (R$)"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
 
           <button className="button" type="submit">Cadastrar</button>
         </form>
       </div>
     </div>
   )
+
 }
