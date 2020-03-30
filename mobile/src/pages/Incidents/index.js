3990a86c9edf3db4db0cb40a73278f5f878d57a1
dejalma <dejalma.arantes@gmail.com>
2020-03-29 22:16:15 -0300

    Add pagination

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -14,6 +14,9 @@ export default function Incidents() {
   const [incidents, setIncidents] = useState([])
   const [total, setTotal] = useState(0)
 
+  const [page, setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
+
   const navigation = useNavigation()
 
   function navigateToDetail(incident) {
@@ -21,10 +24,24 @@ export default function Incidents() {
   }
 
   async function loadIncidents() {
-    const res = await api.get('/incidents')
+    if(loading) {
+      return;
+    }
 
-    setIncidents(res.data)
+    if (total > 0 && incidents.length === total) {
+      return;
+    }
+
+    setLoading(true)
+
+    const res = await api.get('/incidents', {
+      params: { page }
+    })
+
+    setIncidents([...incidents, ...res.data])
     setTotal(res.headers['x-total-count'])
+    setPage(page + 1)
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -47,7 +64,9 @@ export default function Incidents() {
         data={incidents}
         style={styles.incidentList}
         keyExtractor={incident => String(incident.id)}
-        showsVerticalScrollIndicator={false}
+        showsVerticalScrollIndicator={true}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
